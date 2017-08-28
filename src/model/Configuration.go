177e8ef70cb92045718fabc267f2bd60a8ae5639
Jaroslav Tóth <xtothj@gmail.com>
2017-08-28 16:19:50 +0200

    #time 30m #comment Refactored prediction configuration and final data.

--- src/model/Configuration.go
+++ src/model/Configuration.go
@@ -6,14 +6,19 @@ import (
 	"configuration"
 )
 
+type ConfigurationManager struct {}
+
 // The struct of configuration file.
 // Attribute NetworkConfiguration NetworkConfiguration - network-based settings.
 // See NetworkConfiguration.
 // Attribute CleaningConfiguration - settings that relate with periodical cleaning of old data entries.
+// Attribute PredictionConfiguration - setting that relate with smoothing and forecasting model.
+// Attribute RestConfiguration - settings that relate with routing.
 type ConfigData struct {
 	NetworkConfiguration 	NetworkConfiguration
 	CleaningConfiguration 	CleaningConfiguration
 	PredictionConfiguration PredictionConfiguration
+	RestConfiguration		RestConfiguration
 }
 
 // Network-based settings.
@@ -30,7 +35,7 @@ type NetworkConfiguration struct {
 	DataBuffer 			uint
 }
 
-// Prediction-based settings.
+// Cleaning-based settings.
 // Attribute CleaningInterval uint - attribute specifies how often should old data entries be removed (seconds).
 // Attribute CleaningDepth uint - only data entries that are older than this treshhold are removed (seconds).
 type CleaningConfiguration struct {
@@ -38,15 +43,31 @@ type CleaningConfiguration struct {
 	CleaningDepth 		uint
 }
 
+// Prediction-based settings.
+// Attribute SmoothingRange uint - time range (milliseconds) that is smoothed to one point in time.
+// Attribute SmoothingThreads uint - Initial number of threads that serve data smoothing. This count is subsequently
+// decreased if threads cannot be fitted with data slice.
 type PredictionConfiguration struct {
 	SmoothingRange		uint
+	SmoothingThreads	uint
+}
+
+// REST configuration.
+// Attribute LocalhostPort uint - listening TCP port (HTTP communication).
+type RestConfiguration struct {
+	LocalhostPort		uint
+}
+
+func NewConfigurationManager() *ConfigurationManager {
+	return &ConfigurationManager{}
 }
 
 // Parsing of XML configuration file into the ConfigData struct.
 // Returns ConfigData - The struct with all configuration settings. See ConfigData.
-func ReadConfiguration() ConfigData {
-	configuration.OpenXmlConfigurationFile()
-	defer configuration.CloseConfigurationFile()
+func (ConfigurationManager *ConfigurationManager) ReadConfiguration() ConfigData {
+	xmlInstance := configuration.NewConfigFileAccessor()
+	xmlInstance.OpenXmlConfigurationFile()
+	defer xmlInstance.CloseConfigurationFile()
 	xmlFileData, _ := ioutil.ReadAll(configuration.XmlFile)
 	var configData ConfigData
 	err := xml.Unmarshal(xmlFileData, &configData)
--- src/model/FinalData.go
+++ src/model/FinalData.go
@@ -4,7 +4,29 @@ import (
 	"time"
 )
 
+type DataRouter struct {}
+
+// Smoothed or predicted data.
+// Attribute DataElement float64 - number of bytes.
+// Attribute Timestamp time.Time - data element is set on this time.
 type FinalData struct {
 	DataElement		float64
 	Timestamp		time.Time
 }
+
+// Prediction creator - output data slices coupled with data type.
+// Attribute smoothedData *[](*model.FinalData) - smoothing correction of data slices. See model.FinalData.
+// Attribute predictedData *[](*model.FinalData) - prediction up to the specified horizon. See model.FinalData.
+// Attribute *model.DataType - data type to which the prediction is created (individual smoothing is needed too).
+type OutputData struct {
+	smoothedData	*[](*FinalData)
+	predictedData	*[](*FinalData)
+	*DataType
+}
+
+// Creating of DataRouter instance.
+// Returning *DataRouter - instance that allows collecting and analysing of final smoothed and predicted data.
+func NewDataRouter() *DataRouter {
+	return &DataRouter{}
+}
+
