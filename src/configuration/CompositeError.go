febfb4d1f41b606ed4cd8198fba067070fefeb9a
Jaroslav Tóth <xtothj@gmail.com>
2017-09-02 11:30:52 +0200

    #time 10m #comment Repaired errors list - new instances of errors list must be created.

--- src/configuration/CompositeError.go
+++ src/configuration/CompositeError.go
@@ -7,34 +7,48 @@ import (
 	"errors"
 )
 
-// List or buffer of errors that are collected.
-var errorsList []CompositeError = make([]CompositeError, 0, 10)
+// Attribute errorsList []ErrorEntry - list of collected errors.
+type CompositeError struct {
+	errorsList []ErrorEntry
+}
 
 // Error consists of identification (int), description (string), and time (time.Time).
 // Attribute Id int - identification of error.
 // Attribute Description string - description of error.
 // Attribute Time time.Time - Auto-generated time when an error was added to list. See time.Time.
-type CompositeError struct {
+type ErrorEntry struct {
 	Id			int
 	Description	string
 	Time		time.Time
 }
 
+// Creating of the new composite error.
+// Returning *CompositeError - object of CompositeError.
+func NewCompositeError() *CompositeError {
+	return &CompositeError{}
+}
+
 // Adding of a new error to the error buffer.
 // Parameter 'id int' - identification of error (doesn't have to be unique).
 // Parameter 'description string' - description of error.
-func(compositeError *CompositeError) AddError(id int, description string) {
+func(CompositeError *CompositeError) AddError(id int, description string) {
 	actualTime := time.Now()
-	errorsList = append(errorsList, CompositeError{Id: id, Description: description, Time: actualTime})
+	CompositeError.errorsList = append(CompositeError.errorsList, ErrorEntry {
+		Id: id,
+		Description: description,
+		Time: actualTime,
+	})
 }
 
 // Evaluating of error - if the error buffer is not empty, an error is thrown or returned.
-func(compositeError *CompositeError) Evaluate() error {
-	if len(errorsList) != 0 {
+func(CompositeError *CompositeError) Evaluate() error {
+	if len(CompositeError.errorsList) != 0 {
 		var buffer bytes.Buffer
-		for i := range errorsList {
-			line := fmt.Sprintf("%d, %s: %s\n", errorsList[i].Id, errorsList[i].Time,
-				errorsList[i].Description)
+		for i := range CompositeError.errorsList {
+			line := fmt.Sprintf("%d, %s: %s\n",
+				CompositeError.errorsList[i].Id,
+				CompositeError.errorsList[i].Time,
+				CompositeError.errorsList[i].Description)
 			buffer.WriteString(line)
 		}
 		return errors.New(buffer.String())
