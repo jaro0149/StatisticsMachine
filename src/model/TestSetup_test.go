87087057389bec3e8dc83843719ff063763e4d9e
Jaroslav Tóth <xtothj@gmail.com>
2017-09-02 11:30:06 +0200

    #time 10m #comment Central test setup.

---
+++ src/model/TestSetup_test.go
@@ -0,0 +1,43 @@
+package model
+
+import (
+	"testing"
+	"os"
+	"io/ioutil"
+	"configuration"
+)
+
+// Statistical operations.
+var statMachine *StatisticalData
+
+// Database connection.
+var databaseConnection *configuration.DatabaseConnection
+
+// Data router.
+var dataRouter *DataRouter
+
+// Scheduling of setup, unit tests and tear-down functions. See testing.M
+// Parameter m *testing.M - unit tests machine.
+func TestMain(m *testing.M) {
+	setUp()
+	retCode := m.Run()
+	tearDown()
+	os.Exit(retCode)
+}
+
+// Unit tests preparation - initialisation of logging and database unit and tested objects.
+//
+func setUp() {
+	configuration.LoggingInit(ioutil.Discard, os.Stdout, os.Stdout, os.Stderr)
+	databaseConnection = configuration.NewDatabaseConnection()
+	databaseConnection.ConnectDatabaseFromTest(2)
+	statMachine = NewStatisticalData()
+	dataRouter = NewDataRouter()
+}
+
+// Cleaning after performing unit tests - closing of the database connection.
+//
+func tearDown() {
+	cleanDatabases(nil)
+	databaseConnection.CloseDatabase()
+}
